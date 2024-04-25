fb6453c4662432edb7b1be1fcf37824b52373e5d
MiguelAguiarF <miguelaguiarf5@gmail.com>
2024-04-25 08:58:52 -0300

    appJar

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,4 +1,4 @@
-import {View, Image, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useEffect, useState } from "react";
 import aa from '../assets/tulipa.jpg';
 import Det from "../components/Details";
@@ -6,12 +6,16 @@ import LogoBar from '../components/LogoBar';
 
 
 
-export default function Details({routes}){
-
-    return(
+export default function Details({ route }) {
+    const { nome_cientifico, desc, imagem } = route.params;
+    return (
         <View>
             <LogoBar />
-           <Det />
+            <Det
+                image={source={uri:`http://10.0.2.2:3000/assets/${imagem}`}}
+                name={nome_cientifico}
+                description={desc}
+            />
         </View>
 
     );
